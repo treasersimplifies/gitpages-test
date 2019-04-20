2305e300569bf551a46b6e79667fad226f01cb93
treasersimplifies <31153014+treasersimplifies@users.noreply.github.com>
2019-04-20 22:21:12 +0800

    Updates

---
+++ precache-manifest.03e8731ac221f4a89aa70f3c63618eb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gitpages-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "afb061f0547697b6626e",
+    "url": "/gitpages-test/static/js/runtime~main.07365909.js"
+  },
+  {
+    "revision": "4464bbec5165ccd8b942",
+    "url": "/gitpages-test/static/js/main.3a235ff5.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/gitpages-test/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "4464bbec5165ccd8b942",
+    "url": "/gitpages-test/static/css/main.19da4f0b.chunk.css"
+  },
+  {
+    "revision": "67891bd81bf372465c04dcf939dc2b9b",
+    "url": "/gitpages-test/index.html"
+  }
+];
